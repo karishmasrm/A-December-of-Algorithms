2dc8f3cd74413ee77bb1d02a3945b4924a76c2f6
karishmasrm <agarwalkarishma10@gmail.com>
2018-12-27 20:18:53 +0530

    Add files via upload

---
+++ December-30/java_karishma30.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Diagonals
+{
+    public static void main(String args[])
+    { 
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int d=(int)(n*(n-3)/2);
+        System.out.println(d);
+    } 
+    }
